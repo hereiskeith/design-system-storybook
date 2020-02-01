b01e6aded758881d33654305506e1734f284dff2
unknown <hereiskeith@gmail.com>
2020-02-01 16:22:38 -0500

    add docs addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
 import { GlobalStyle } from '../src/shared/global';
+import { withA11y } from '@storybook/addon-a11y';
 
+addDecorator(withA11y);
 addDecorator(story => (
-  <>
+    <>
     <GlobalStyle />
     {story()}
   </>
 ));
+
+configure([
+  require.context('../src', false, /Intro\.stories\.mdx/),
+  require.context('../src', true, /\.stories\.(js|mdx)$/),
+], module);
